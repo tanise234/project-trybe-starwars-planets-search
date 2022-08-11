f2d7753564bf980a3b298566382683d0e617b8ea
Tanise Santos <tanise234@gmail.com>
2022-08-10 22:16:33 -0300

    Testes finalizados (req 5 e 8)

--- src/services/getData.jsx
+++ src/services/getData.jsx
@@ -3,8 +3,8 @@ const getData = () => {
   const data = fetch(endPoint)
     .then((response) => response.json())
     .then((response) => response.results)
-    .then((response) => response.map(({ residents, ...otherKeys }) => otherKeys))
-    .catch((error) => error);
+    .then((response) => response.map(({ residents, ...otherKeys }) => otherKeys));
+    // .catch((error) => error);
   return data;
 };
 
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -70,14 +70,14 @@ describe('Testar as funções', () => {
     expect(planets).toHaveLength(10);
 
     userEvent.type(inputName, "n");
-    expect(planets[0]).toHaveTextContent('Tatooine');
-    expect(screen.getAllByTestId('planet-name')[3]).toHaveTextContent('Bespin');
+    expect(planets[0]).toHaveTextContent('Alderaan');
+    expect(screen.getAllByTestId('planet-name')[3]).toHaveTextContent('Endor');
     userEvent.type(inputName, "d");
     expect(screen.getAllByTestId('planet-name')[0]).toHaveTextContent('Endor');
 
     userEvent.selectOptions(screen.getByTestId('column-filter'), ['rotation_period']);
     userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['maior que']);
-    userEvent.type(screen.getByTestId('value-filter'), '23');
+    userEvent.type(screen.getByTestId('value-filter'), '{backspace} 23');
     expect(screen.getByTestId('column-filter')).toHaveValue('rotation_period');
     expect(screen.getByTestId('comparison-filter')).toHaveValue('maior que');
     expect(screen.getByTestId('value-filter')).toHaveValue(23);
@@ -90,17 +90,21 @@ describe('Testar as funções', () => {
     expect(screen.getAllByTestId('planet-name')).toHaveLength(10);
     userEvent.selectOptions(screen.getByTestId('column-filter'), ['rotation_period']);
     userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['igual a']);
-    userEvent.type(screen.getByTestId('value-filter'), '24');
+    userEvent.type(screen.getByTestId('value-filter'), '{backspace} {backspace} 24');
     userEvent.click(btnAdd);
-    expect(screen.queryAllByTestId('planet-name')).toHaveLength(0);
+    expect(screen.queryAllByTestId('planet-name')).toHaveLength(3);
 
-    const filterPopulation = screen.getByText('population');
+    userEvent.click(btnDel);
+    expect(screen.queryAllByTestId('filter')).toHaveLength(0);
+    expect(screen.getAllByTestId('planet-name')).toHaveLength(10);
+    userEvent.selectOptions(screen.getByTestId('column-filter'), ['rotation_period']);
+    userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['menor que']);
+    userEvent.click(btnAdd);
+    expect(screen.queryAllByTestId('planet-name')).toHaveLength(5);
 
     userEvent.click(btnAdd);
     expect(screen.getAllByTestId('filter')).toHaveLength(2);
-    // expect(filterPopulation).toBeInTheDocument();
-    userEvent.click(btnAdd);
-    expect(screen.getAllByTestId('filter')).toHaveLength(3);
+    const filterPopulation = screen.getByText('population menor que 24');
     
     userEvent.click(btnDel);
     expect(filterPopulation).not.toBeInTheDocument();
@@ -111,6 +115,19 @@ describe('Testar as funções', () => {
     expect(btnDelOne).toHaveLength(2);
     userEvent.click(btnDelOne[0]);
     expect(screen.getAllByTestId('filter')).toHaveLength(1);
+
+    userEvent.click(btnDel);
+    userEvent.click(screen.getByTestId('column-sort-input-desc'));
+    userEvent.click(screen.getByTestId('column-sort-button'));
+    expect(screen.getAllByTestId('planet-name')[0]).toHaveTextContent('Coruscant');
+    expect(screen.getAllByTestId('planet-name')[9]).toHaveTextContent('Hoth');
+
+    userEvent.selectOptions(screen.getByTestId('column-sort'), ['rotation_period']);
+    userEvent.click(screen.getByTestId('column-sort-input-asc'));
+    userEvent.click(screen.getByTestId('column-sort-button'));
+    expect(screen.getAllByTestId('planet-name')[0]).toHaveTextContent('Bespin');
+    expect(screen.getAllByTestId('planet-name')[9]).toHaveTextContent('Kamino');
+
   });
 });
 
